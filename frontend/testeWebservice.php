204f4db8e65a4e7b061ecd0abaf0ea45e73386e4
Anderson Barretto <abarretto78@gmail.com>
2012-04-18 00:15:34 +0100

    FrontEnd. The Begin.

---
+++ frontend/testeWebservice.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Strict//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Teste Web Service</title>
+</head>
+<body>
+<h1>Teste Web Service</h1>
+<form method="put" action="http://localhost/AW3/webservice/noticias.php/2">
+	<textarea cols="100" rows="20" name="xml"></textarea>
+	<br>
+	<input type="submit">
+</form>
+</body>
+</html>
