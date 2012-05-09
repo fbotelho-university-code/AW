c4d68b6be95e824d714b157ea54607244e093873
Jose Lopes <zemasa@gmail.com>
2012-05-09 21:52:31 +0100

    asdads

--- backend/testefonte.php
+++ backend/testefonte.php
@@ -36,6 +36,7 @@ $xml = "<xml>
 		</search>
 	</uri>
 	<noticia>
+		<item>/rss/channel/item</item>
 		<data_pub>item/pubDate</data_pub>
 		<assunto>item/title</assunto>
 		<descricao>item/description</descricao>
@@ -43,13 +44,56 @@ $xml = "<xml>
 	</noticia>
 </xml>";
 
-
 $f = new Fonte();
+$f->data_pub = "item/pubDate";
+$f->assunto = "item/title";
+$f->descricao = "item/description";
+$f->{'link'} = "item/link";
+$f->idfonte = 99;
+$f->item = "/rss/channel/item";
 
 
-
-
-
-
+			//prepara query
+			$url_search = "http://pesquisa.sapo.pt/?barra=noticias&cluster=0&format=rss&location=pt&st=local&limit=10&q=porto";
+			  
+			$ch = curl_init($url_search);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			$data = curl_exec($ch);
+			curl_close($ch);
+			
+			$doc = new SimpleXmlElement($data, LIBXML_NOCDATA);
+			
+			$doc = $data->xpath($f->item);
+			
+			if($doc){
+				foreach($doc as $item){
+					$myNew = new Noticia();
+					$myNew->setIdfonte($f->idfonte);
+					
+					// pubDate
+					$pub = $item->xpath($f->data_pub);
+					if(isset($pub)) $myNew->setData_pub(Util::formatDateToDB($pub));
+					else $myNew->setData_pub("");
+					
+					// assunto
+					$ass = $item->xpath($f->assunto);
+					if(isset($ass)) $myNew->setAssunto($ass);
+					else $myNew->setAssunto("");
+					
+					// descricao
+					$desc = $item->xpath($f->descricao);
+					if(isset($desc)) $myNew->setDescricao($desc);
+					else $myNew->setDescricao("");
+					
+					// link
+					$link = $item->xpath($f->{'link'});
+					if(isset($link)) $myNew->setLink($link);
+					else continue;
+					
+					// texto
+					//parserNoticias
+				}
+				echo "Foram inseridas notícias da Fonte ".$this->getNome()." com sucesso.";
+			}
 
 ?>
